9d7b0853613f24066c0f9bc6028a2e0b01e647a7
Christopher DeBoever <cdeboever3@gmail.com>
2014-06-12 10:49:03 -0700

    Added anothe neg strand test in star module
    Also updated my fake external annotation to have a neg strand gene.

--- tests/star/test_star.py
+++ tests/star/test_star.py
@@ -14,24 +14,31 @@ import cdpybio as cpb
 # results aren't sensitive to strand. I could also test the define_sample_name
 # functionality.
 
-EXTDF = pd.DataFrame([['gene1', 'chr1', 10, 20, '+', 'chr1:10', 'chr1:20',
-                       'chr1:10:+', 'chr1:20:+', 'chr1:10-20'],
-                      ['gene1', 'chr1', 5, 25, '+', 'chr1:5', 'chr1:25',
-                       'chr1:5:+', 'chr1:25:+', 'chr1:5-25'],
-                      ['gene1', 'chr1', 2, 20, '+', 'chr1:2', 'chr1:20',
-                       'chr1:2:+', 'chr1:20:+', 'chr1:2-20'],
-                      ['gene1', 'chr1', 5, 20, '+', 'chr1:5', 'chr1:20',
-                       'chr1:5:+', 'chr1:20:+', 'chr1:5-20']],
-                     index=['chr1:10-20:+', 'chr1:5-25:+', 'chr1:2-20:+',
-                            'chr1:5-20:+'],
-                     columns=['gene', 'chrom', 'start', 'end', 'strand',
-                              'chr:start', 'chr:end', 'donor', 'acceptor',
-                              'intron'])
-
 class TestMisc:
     def test_read_ext(self):
-        df = cpb.star.read_external_annotation('ext.tsv')
-        assert_frame_equal(df, EXTDF)
+        vals = [['gene1', 'chr1', 10, 20, '+', 'chr1:10', 'chr1:20',
+                 'chr1:10:+', 'chr1:20:+', 'chr1:10-20'],
+                ['gene1', 'chr1', 5, 25, '+', 'chr1:5', 'chr1:25', 'chr1:5:+',
+                 'chr1:25:+', 'chr1:5-25'],
+                ['gene1', 'chr1', 2, 20, '+', 'chr1:2', 'chr1:20', 'chr1:2:+',
+                 'chr1:20:+', 'chr1:2-20'],
+                ['gene1', 'chr1', 5, 20, '+', 'chr1:5', 'chr1:20', 'chr1:5:+',
+                 'chr1:20:+', 'chr1:5-20'],
+                ['gene2', 'chr2', 10, 20, '-', 'chr2:10', 'chr2:20',
+                 'chr2:20:-', 'chr2:10:-', 'chr2:10-20'],
+                ['gene2', 'chr2', 5, 25, '-', 'chr2:5', 'chr2:25', 'chr2:25:-',
+                 'chr2:5:-', 'chr2:5-25'],
+                ['gene2', 'chr2', 2, 20, '-', 'chr2:2', 'chr2:20', 'chr2:20:-',
+                 'chr2:2:-', 'chr2:2-20'],
+                ['gene2', 'chr2', 5, 20, '-', 'chr2:5', 'chr2:20', 'chr2:20:-',
+                 'chr2:5:-', 'chr2:5-20']]
+        ind = [u'chr1:10-20:+', u'chr1:5-25:+', u'chr1:2-20:+', u'chr1:5-20:+',
+               u'chr2:10-20:-', u'chr2:5-25:-', u'chr2:2-20:-', u'chr2:5-20:-']
+        cols=['gene', 'chrom', 'start', 'end', 'strand', 'chr:start', 'chr:end',
+              'donor', 'acceptor', 'intron']
+        df = pd.DataFrame(vals, index=ind, columns=cols)
+        df2 = cpb.star.read_external_annotation('ext.tsv')
+        assert_frame_equal(df, df2)
 
     def test_read_sj_out_pos(self):
         vals = [['chr1', 2, 20, '+', 'GT/AG', True, 5, 1, 10],
@@ -193,7 +200,7 @@ class TestMakeSJOutPanel:
         assert_panel_equal(p, p2)
 
 class TestFilterJxnsDonorAcceptor:
-    def test_filter_jxns_donor_acceptor(self):
+    def test_filter_jxns_donor_acceptor_pos(self):
         d = cpb.star.make_sj_out_dict(['SJ.out.tab.nonew_a',
                                        'SJ.out.tab.nonew_b'])
         p, a = cpb.star.make_sj_out_panel(d)
@@ -216,6 +223,34 @@ class TestFilterJxnsDonorAcceptor:
                          index=[u'chr1:5-20:+', u'chr1:5-25:+',
                                 u'chr1:10-20:+'],
                          columns=[u'SJ.out.tab.nonew_a', u'SJ.out.tab.nonew_b'])
+
+        assert_frame_equal(a, a2)
+        assert_frame_equal(c, c2)
+    
+    def test_filter_jxns_donor_acceptor_neg(self):
+        d = cpb.star.make_sj_out_dict(['SJ.out.tab.neg_nonew_a',
+                                       'SJ.out.tab.neg_nonew_b'])
+        p, a = cpb.star.make_sj_out_panel(d)
+        ext = cpb.star.read_external_annotation('ext.tsv')
+        c2, a2 = cpb.star.filter_jxns_donor_acceptor(p, a, ext)
+        a = pd.DataFrame(
+            [['chr2', 5, 20, '-', 'GT/AG', True, True, 'chr2:5', 
+              'chr2:20', 'gene2', 'chr2:20:-', 'chr2:5:-', False, False], 
+             ['chr2', 5, 25, '-', 'CT/AC', True, True, 'chr2:5', 
+              'chr2:25', 'gene2', 'chr2:25:-', 'chr2:5:-', False, False], 
+             ['chr2', 10, 20, '-', 'CT/AC', True, True, 'chr2:10', 
+              'chr2:20', 'gene2', 'chr2:20:-', 'chr2:10:-', False, False]],
+            index=[u'chr2:5-20:-', u'chr2:5-25:-', u'chr2:10-20:-'],
+            columns=[u'chrom', u'start', u'end', 
+                     u'strand', u'intron_motif', u'annotated', 
+                     u'ext_annotated', u'chr:start', u'chr:end', 
+                     u'gene_id', u'donor', u'acceptor', u'novel_donor', 
+                     u'novel_acceptor'])
+        c = pd.DataFrame(array([[20,  0],[10, 10],[20, 20]]),
+                         index=[u'chr2:5-20:-', u'chr2:5-25:-',
+                                u'chr2:10-20:-'],
+                         columns=[u'SJ.out.tab.neg_nonew_a', 
+                                  u'SJ.out.tab.neg_nonew_b'])
 
         assert_frame_equal(a, a2)
         assert_frame_equal(c, c2)
