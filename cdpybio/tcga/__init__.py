b1985a05c766013a5b70c91a796f24899d2dd252
Chris DeBoever <cdeboever3@gmail.com>
2014-09-19 15:52:37 -0700

    setup.py update for firehose_get and general info
    I've updated setup.py to include a license etc. I've also added a
    post-install script that will download firehose_get.

--- cdpybio/tcga/__init__.py
+++ cdpybio/tcga/__init__.py
@@ -1,1 +1,5 @@
 import Data, Figures, Initialization, Processing, Stats
+
+firehose_get = os.path.join(
+    os.path.sep.join(p.split(os.path.sep)[0:-2]), 'bin', 'firehose_get'
+)
--- setup.py
+++ setup.py
@@ -1,29 +1,67 @@
 import os
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+def post(command_subclass):
+    """A decorator for classes subclassing one of the setuptools commands.
+
+    It modifies the run() method to download firehose_get
+    """
+    orig_run = command_subclass.run
+
+    def modified_run(self):
+        import shutil
+        from subprocess import check_call
+        from urllib2 import urlopen
+        src = 'http://gdac.broadinstitute.org/runs/code/firehose_get_latest.zip'
+        req = urlopen(src)
+        dest = 'bin/firehose_get_latest.zip'
+        try:
+            os.makedirs('bin')
+        except OSError:
+            pass
+        with open(dest, 'w') as d:
+            shutil.copyfileobj(req, d)
+        check_call(['unzip', dest, '-d', 'bin'])
+        os.remove(dest)
+        orig_run(self)
+
+    command_subclass.run = modified_run
+    return command_subclass
+
+@post
+class CustomDevelopCommand(develop):
+    pass
+
+@post
+class CustomInstallCommand(install):
+    pass
+
 setup(
     name = 'cdpybio',
-    version = '0.0.1',
+    version = '0.1.0',
     author = 'Christopher DeBoever',
     author_email = 'cdeboever3@gmail.com',
-    description = ('''A module that contains different useful things I like to
-                   use.'''),
+    description = ('A module that contains different useful things I like to '
+                   'use.'),
     packages=find_packages(),
+    cmdclass={
+        'install' : CustomInstallCommand,
+        'develop' : CustomDevelopCommand,
+    },
+    license = 'MIT',
+    keywords = 'bioinformatics',
+    url = 'https://github.com/cdeboever3/cdpybio',
+    long_description=read('README.md'),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+   ]
 )
-#     license = 'BSD',
-#     keywords = 'example documentation tutorial',
-#     url = 'http://packages.python.org/an_example_pypi_project',
-#     packages=['an_example_pypi_project', 'tests'],
-#     long_description=read('README'),
-#     classifiers=[
-#         'Development Status :: 3 - Alpha',
-#         'Topic :: Utilities',
-#         'License :: OSI Approved :: BSD License',
-#    ],
