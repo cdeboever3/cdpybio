0e3b19f5284cab9021f4ff6a098031227e33ef54
Chris DeBoever <cdeboever3@gmail.com>
2014-09-23 15:55:25 -0700

    Added some filters for mutect data

--- cdpybio/mutect.py
+++ cdpybio/mutect.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import pybedtools as pbt
 
-def read_variants(fns, remove=['DBSNP'], keep_only=True ):
+def read_variants(fns, remove=['DBSNP'], keep_only=True,
+                  min_tumor_f=0.1, min_tumor_cov=14,
+                  min_normal_cov=8):
     """Read muTect results from the list of files fns
 
     Parameters
@@ -21,6 +23,15 @@ def read_variants(fns, remove=['DBSNP'], keep_only=True ):
         If True, only keep variants with 'KEEP' in "judgement" column.
         Otherwise, keep all variants.
 
+    min_tumor_f : float between 0 and 1
+        Minimum tumor allelic fraction.
+
+    min_tumor_cov : int > 0
+        Minimum coverage of the variant in the tumor.
+
+    min_normal_cov : int > 0
+        Minimum coverage of the variant in the normal.
+
     """
     variants = []
     for i, f in enumerate(fns):
@@ -31,6 +42,9 @@ def read_variants(fns, remove=['DBSNP'], keep_only=True ):
             tdf = tdf[tdf.dbsnp_site != t]
         if keep_only:
             tdf = tdf[tdf.judgement == 'KEEP']
+        tdf = tdf[tdf.tumor_f > min_tumor_f]
+        tdf = tdf[tdf.t_ref_count + tdf.t_alt_count > min_tumor_cov]
+        tdf = tdf[tdf.n_ref_count + tdf.n_alt_count > min_nromal_cov]
         variants.append(tdf)
     variants = pd.concat(variants)
     variants.index = range(variants.shape[0])
