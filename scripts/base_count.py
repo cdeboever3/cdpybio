b41308d6dd5a97c8b73c334a098b851fff6340ec
Chris DeBoever <cdeboever3@gmail.com>
2015-03-01 17:16:49 -0800

    Updates to nucleotide counting script

--- scripts/base_count.py
+++ scripts/base_count.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 
-import pysam
+import cdpybio as cpb
 
 def main():
     parser = argparse.ArgumentParser(
@@ -13,19 +13,35 @@ def main():
                                      'be named the same as the bam but with '
                                      '.bai appended to the end of the filename '
                                      '(e.g. XXX.bam and XXX.bam.bai.'))
+    parser.add_argument('counts', help=('Output tsv file for nucleotide '
+                                        'counts.'))
     parser.add_argument('--vcf', 
                         help='VCF file defining positions to be counted.')
     parser.add_argument('--bed', 
                         help='Bed file defining positions to be counted.')
+    parser.add_argument('--stranded', 
+                        help='Count nucleotides in a strand-specific manner.',
+                        action=store_true)
     args = parser.parse_args()
 
     bam = args.bam
+    counts = args.counts
     vcf = args.vcf
     bed = args.bed
+    stranded = args.stranded
 
     # assert os.path.exists(bam + '.bai'), 'can\'t find bam index file'
     assert vcf or bed, 'must specify VCF or bed file'
     assert not (vcf and bed), 'cannot provide both VCF and bed files'
+
+    if vcf:
+        positions = vcf
+    elif bed:
+        positions = bed
+
+    df = cpb.pysam.nt_counts(bam, positions, stranded=stranded, vcf=vcf,
+                             bed=bed)
+    df.to_csv(counts, sep='\t')
 
 if __name__ == '__main__':
     main()
