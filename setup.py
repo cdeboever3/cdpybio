f5de17c2ee982eb8b28e154416a74fe34f18c1c9
Christopher DeBoever <cdeboever3@gmail.com>
2015-11-13 13:47:55 -0800

    Fix for pypi
    Still trying to get README.md -> README.rst working.

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 import os
 from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 try:
     import pypandoc
     long_description = pypandoc.convert('README.md', 'rst', format='md')
 except(IOError, ImportError):
-    long_description = read('README.md')
+    long_description = open('README.md').read()
 
 setup(
     name = 'cdpybio',
     packages=['cdpybio'],
-    version = '0.0.4',
+    version = '0.0.5',
     author = 'Christopher DeBoever',
     author_email = 'cdeboever3@gmail.com',
     description = ('A collection of useful methods for working with various '
@@ -21,7 +18,7 @@ setup(
     license = 'MIT',
     keywords = ['bioinformatics'],
     url = 'https://github.com/cdeboever3/cdpybio',
-    download_url = 'https://github.com/cdeboever3/cdpybio/tarball/0.0.4',
+    download_url = 'https://github.com/cdeboever3/cdpybio/tarball/0.0.5',
     long_description=long_description,
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
